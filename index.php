48342eb88785130a8c85ae751bce5223f985c236
Razanius12 <razanius12@gmail.com>
2024-11-04 16:05:50 +0700

    bulk changes phase 1
    - added index.php
    - added head.php (still bug on icon)
    - added sidebar.php (unfinished)
    - some href still uses the "../"
    - added documentation
    - added alf logo

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include 'head.php';
+?>
+
+<body>
+ <div class="wrapper">
+ <?php
+ include 'sidebar.php';
+ ?>
+ </div>
+
+ <?php
+ include 'script.php';
+ ?>
+
+</body>
