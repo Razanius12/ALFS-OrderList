198cafa3b68f2fb4d6aaf354c125e81818d89903
FAUZAN <fauzanalfsolution4@gmail.com>
2024-11-27 18:28:54 +0700

    Update footer.php
    perubahan ke 2

--- footer.php
+++ footer.php
@@ -3,9 +3,7 @@
   <nav class="pull-left">
    <ul class="nav">
     <li class="nav-item">
-     <a class="nav-link" href="http://www.themekita.com">
-      ThemeKita
-     </a>
+     <a class="nav-link" href="http://www.themekita.com">ThemeKita</a>
     </li>
     <li class="nav-item">
      <a class="nav-link" href="#"> Help </a>
@@ -15,15 +13,13 @@
     </li>
    </ul>
   </nav>
-  <div class="d-flex align-items-center">
-   <div class="copyright me-3">
-    2024, made by Razanius12 and Fauzan <i class=""></i> by
-    <a href="http://www.themekita.com">ThemeKita</a>
-   </div>
-   <div>
-    Distributed by
-    <a target="_blank" href="https://themewagon.com/">ThemeWagon</a>.
-   </div>
+  <div class="text-center flex-grow-1">
+   2024, made by <a href="https://github.com/Razanius12" style="color: blue;">Razanius12</a> and <a href="https://github.com/Fauzan" style="color: blue;">Fauzan</a> <i class=""></i> by
+   <a href="http://www.themekita.com">ThemeKita</a>
+  </div>
+  <div>
+   Distributed by
+   <a target="_blank" href="https://themewagon.com/">ThemeWagon</a>.
   </div>
  </div>
 </footer>
