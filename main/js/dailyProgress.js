7edbdeb79d0130507238700d8a807e617a3d0099
FAUZAN <fauzanalfsolution4@gmail.com>
2024-11-29 14:42:56 +0700

    update daily progress

--- main/js/dailyProgress.js
+++ main/js/dailyProgress.js
@@ -1,49 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
 
- $(document).ready(function () {
+ // Destroy existing chart if it exists
+ if (window.dailyProgressBarChart instanceof Chart) {
+  window.dailyProgressBarChart.destroy();
+ }
 
-  // Destroy existing chart if it exists
-  if (window.dailyProgressBarChart instanceof Chart) {
-   window.dailyProgressBarChart.destroy();
-  }
+ // Get the canvas element
+ const ctx = document.getElementById('dailyProgressBarChart').getContext('2d');
 
-  // Get the canvas element
-  const ctx = document.getElementById('dailyProgressBarChart').getContext('2d');
-
-  // Create the chart instance
-  window.dailyProgressBarChart = new Chart(ctx, {
-   type: 'bar', // or any other chart type
-   data: {
-    labels: [
-     "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-    ],
-    datasets: [
-     {
-      label: "Clients",
-      backgroundColor: "rgb(23, 125, 255)",
-      borderColor: "rgb(23, 125, 255)",
-      data: [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4],
-     },
-    ],
-   },
-   options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-     y: {
-      beginAtZero: true,
-      min: 0, // Set minimum value for y-axis
-      max: 10, // Set maximum value for y-axis
-      ticks: {
-       stepSize: 1, // Set the interval between ticks
-       display: true // Ensure ticks are displayed
+ // Create the chart instance
+ window.dailyProgressBarChart = new Chart(ctx, {
+  type: 'bar', // or any other chart type
+  data: {
+   labels: [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+   ],
+   datasets: [
+    {
+     label: "Clients",
+     backgroundColor: "rgb(23, 125, 255)",
+     borderColor: "rgb(23, 125, 255)",
+     data: [1, 2, 5, 6, 5, 4, 6, 4, 6, 7, 8, 7, 0],
+    },
+   ],
+  },
+  options: {
+   responsive: true,
+   maintainAspectRatio: false,
+   scales: {
+    y: {
+     beginAtZero: true, // Pastikan sumbu y mulai dari 0
+     min: 0, // Set nilai minimum untuk sumbu y ke 0
+     ticks: {
+      callback: function (value) {
+       return value; // Menampilkan nilai tick
       }
      }
-    },
-   },
-  });
-
+    }
+   }
+  },
  });
-
 });
