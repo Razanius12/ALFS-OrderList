eb44dd7af1c9b52100536fe632c1fa1bc902e3ba
FAUZAN <fauzanalfsolution4@gmail.com>
2024-11-29 15:44:37 +0700

    alf offices proses

--- main/pages/alfOffices.php
+++ main/pages/alfOffices.php
@@ -1,49 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <title>ALF Offices</title>
+</head>
+<body>
 <div class="container">
- <div class="page-inner">
-  <div class="page-header mb-0">
-   <h3 class="fw-bold mb-3">ALF Offices</h3>
-   <ul class="breadcrumbs mb-3">
-    <li class="nav-home">
-     <a href="./index.php">
-      <i class="icon-home"></i>
-     </a>
-    </li>
-    <li class="separator">
-     <i class="icon-arrow-right"></i>
-    </li>
-    <li class="nav-item">
-     <a href="./index.php?page=alfOffices">ALF Offices</a>
-    </li>
-   </ul>
-  </div>
-  <div class="page-category">Shows all available ALF Solution offices</div>
-  <div class="row">
-   <div class="col-md-12">
-    <div class="card">
-     <div class="card-header">
-      <div class="card-title">Bandung</div>
-     </div>
-     <div class="card-body">
-      <iframe
-       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.735343559108!2d107.72001217204199!3d-6.922208243794203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68dcd4f3fa4d39%3A0xce64b9d681e418e3!2sAlfsolution%20Office!5e0!3m2!1sen!2sid!4v1730811639970!5m2!1sen!2sid"
-       width="600" height="450" style="border:0; width:100%" allowfullscreen="" loading="lazy"
-       referrerpolicy="no-referrer-when-downgrade"></iframe>
-     </div>
+    <div class="page-inner">
+        <div class="page-header mb-0">
+            <h3 class="fw-bold mb-3">ALF Offices</h3>
+            <ul class="breadcrumbs mb-3">
+                <li class="nav-home">
+                    <a href="./index.php">
+                        <i class="icon-home"></i>
+                    </a>
+                </li>
+                <li class="separator">
+                    <i class="icon-arrow-right"></i>
+                </li>
+                <li class="nav-item">
+                    <a href="./index.php?page=alfOffices">ALF Offices</a>
+                </li>
+            </ul>
+        </div>
+        <div class="page-category">Shows all available ALF Solution offices</div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between align-items-center">
+                        <div class="card-title">Bandung</div>
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#addOfficeModal">Tambah</button>
+                    </div>
+                    <div class="card-body">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.735343559108!2d107.72001217204199!3d-6.922208243794203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68dcd4f3fa4d39%3A0xce64b9d681e418e3!2sAlfsolution%20Office!5e0!3m2!1sen!2sid!4v1730811639970!5m2!1sen!2sid"
+                            width="600" height="450" style="border:0; width:100%" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title">Garut</div>
+                    </div>
+                    <div class="card-body">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.639651324924!2d107.9477189741453!3d-7.167588770328902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68b11b8104ff0b%3A0x330b1a00f1a74129!2sKantor%20ALF%20Solution%20Cabang%20Garut!5e0!3m2!1sen!2sid!4v1730812164569!5m2!1sen!2sid"
+                            width="600" height="450" style="border:0; width:100%" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-   </div>
-   <div class="col-md-12">
-    <div class="card">
-     <div class="card-header">
-      <div class="card-title">Garut</div>
-     </div>
-     <div class="card-body">
-      <iframe
-       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.639651324924!2d107.9477189741453!3d-7.167588770328902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68b11b8104ff0b%3A0x330b1a00f1a74129!2sKantor%20ALF%20Solution%20Cabang%20Garut!5e0!3m2!1sen!2sid!4v1730812164569!5m2!1sen!2sid"
-       width="600" height="450" style="border:0; width:100%" allowfullscreen="" loading="lazy"
-       referrerpolicy="no-referrer-when-downgrade"></iframe>
-     </div>
-    </div>
-   </div>
-  </div>
- </div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="addOfficeModal" tabindex="-1" role="dialog" aria-labelledby="addOfficeModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addOfficeModalLabel">Tambah Kantor
